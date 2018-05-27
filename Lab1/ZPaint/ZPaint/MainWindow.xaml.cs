0b7223f900fc5135e12421d3cca8e562320e756e
zeiflonte <zeiflonte@gmail.com>
2018-05-27 13:13:30 +0300

    Added XML, canvas background color, reorganized folder structure

--- Lab1/ZPaint/ZPaint/MainWindow.xaml.cs
+++ Lab1/ZPaint/ZPaint/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ using System.Runtime.Serialization.Json;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace ZPaint
 {
@@ -41,6 +43,18 @@ namespace ZPaint
         private int thickness;
         private SolidColorBrush color;
 
+        SolidColorBrush BackgroundColor
+        {
+            get
+            {
+                return (SolidColorBrush)canvas.Background;
+            }
+            set
+            {
+                canvas.Background = value;
+            }
+        }
+
         Dictionary<string, Factory> Plugins = new Dictionary<String, Factory>();
         private int amountOfPlugins = 0;
 
@@ -51,6 +65,7 @@ namespace ZPaint
         {
             InitializeComponent();
             addPlugins();
+            XMLLoad();
         }
 
         // Choose a type of a figure
@@ -327,6 +342,68 @@ namespace ZPaint
                     break;
                 }
             }
+        }
+
+        void XMLconstuct()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlDeclaration xmlDeclaration = xmlDocument.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = xmlDocument.DocumentElement;
+            xmlDocument.InsertBefore(xmlDeclaration, root);
+
+            XmlElement body = xmlDocument.CreateElement(string.Empty, "body", string.Empty);
+            xmlDocument.AppendChild(body);
+
+            XmlElement background = xmlDocument.CreateElement(string.Empty, "background", string.Empty);
+            XmlText backgroundColor = xmlDocument.CreateTextNode(canvas.Background.ToString());
+            body.AppendChild(background);
+            background.AppendChild(backgroundColor);
+
+            xmlDocument.Save("../../document.xml");
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //XMLconstuct();
+        }
+
+        void XMLLoad()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load("../../document.xml");
+           
+            XmlNodeList backgroundColor = xmlDocument.GetElementsByTagName("background");
+            // BackgroundColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString(backgroundColor[0].InnerText));
+            cbBackgroundColor.Text = backgroundColor[0].InnerText;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlDeclaration xmlDeclaration = xmlDocument.CreateXmlDeclaration("1.0", "UTF-8", null);
+            XmlElement root = xmlDocument.DocumentElement;
+            xmlDocument.InsertBefore(xmlDeclaration, root);
+
+            XmlElement body = xmlDocument.CreateElement(string.Empty, "body", string.Empty);
+            xmlDocument.AppendChild(body);
+
+            XmlElement background = xmlDocument.CreateElement(string.Empty, "background", string.Empty);
+            XmlText backgroundColor = xmlDocument.CreateTextNode((String)((ComboBoxItem)cbBackgroundColor.SelectedItem).Content);
+            body.AppendChild(background);
+            background.AppendChild(backgroundColor);
+
+            xmlDocument.Save("../../document.xml");
+        }
+
+        private void cbBackgroundColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var dic = new Dictionary<String, SolidColorBrush>();
+            dic.Add("White", new SolidColorBrush(Colors.White));
+            dic.Add("Green", new SolidColorBrush(Colors.Green));
+            dic.Add("Yellow", new SolidColorBrush(Colors.Yellow));
+            dic.Add("Violet", new SolidColorBrush(Colors.Violet));
+            String selectedValue = (String)((ComboBoxItem)cbBackgroundColor.SelectedItem).Content;
+            BackgroundColor = dic[selectedValue];
         }
 
         private void mitReload_Click(object sender, RoutedEventArgs e)
